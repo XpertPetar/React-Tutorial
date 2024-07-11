acab38b0aa46e0b4b7ede8644b7c94af7ed18874
Petar Josifoski <josifoski.petar@gmail.com>
2024-07-11 21:25:04 +0200

    add editEmployee modal functionality

--- src/App.js
+++ src/App.js
@@ -5,72 +5,84 @@ import { v4 as uuidv4 } from "uuid";
 
 function App() {
     const showEmloyees = true;
-    //const [role, setRole] = useState("Dev");
+
     const [employees, setEmployees] = useState([
         {
+            id: 1,
             name: "Petar",
             role: "Admin",
             img: "https://images.pexels.com/photos/3778966/pexels-photo-3778966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
         {
+            id: 2,
             name: "Stefan",
             role: "Dev",
             img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
         {
+            id: 3,
             name: "Random",
             role: "",
             img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
         },
         {
+            id: 4,
             name: "Baci",
             role: "Bot",
             img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
         {
+            id: 5,
             name: "Random",
             role: "",
             img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
         },
         {
+            id: 6,
             name: "Zoki",
             role: "Dev",
             img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
         {
+            id: 7,
             name: "Bazo",
             role: "Tester",
             img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         },
         {
+            id: 8,
             name: "Random",
             role: "",
             img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
         }
     ]);
+
+    function updateEmployee(id, newName, newRole) {
+        const updatedEmployees = employees.map((employee) => {
+            if (employee.id == id) {
+                return { ...employee, name: newName, role: newRole };
+            } else return employee;
+        });
+
+        setEmployees(updatedEmployees);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 {showEmloyees ? (
                     <>
-                        {/* <label for="roleInput">Input Stefan's role: </label>
-                        <input
-                            class="border-2 rounded"
-                            id="roleInput"
-                            onChange={(e) => {
-                                setRole(e.target.value);
-                            }}
-                        ></input> */}
-
                         <div className="p-5 flex flex-wrap gap-2 justify-center">
                             {employees.map((employee) => {
                                 return (
                                     <Employee
-                                        key={uuidv4()}
+                                        key={employee.id}
+                                        id={employee.id}
                                         name={employee.name}
                                         role={employee.role}
                                         img={employee.img}
                                         alt="Employee Image"
+                                        updateEmployee={updateEmployee}
                                     ></Employee>
                                 );
                             })}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,8 +1,10 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
     return (
-        <div className="h-64 w-3/4 m-3 justify-center py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="h-[200px] w-[350px] m-3 justify-center py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
-                className="block mx-auto h-24 object-cover h-[100px] w-[100px] rounded-full sm:mx-0 sm:shrink-0"
+                className="block h-24 object-cover h-[100px] w-[100px] rounded-full sm:mx-0 sm:shrink-0"
                 src={props.img}
             />
             <div className="text-center space-y-2 sm:text-left">
@@ -10,9 +12,12 @@ function Employee(props) {
                     <p className="text-lg text-black font-semibold">{props.name}</p>
                     <p className="text-slate-500 font-medium">{props.role ? props.role : "N/A"}</p>
                 </div>
-                <button className="px-4 py-1 w-full text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Update
-                </button>
+                <EditEmployee
+                    id={props.id}
+                    name={props.name}
+                    role={props.role}
+                    updateEmployee={props.updateEmployee}
+                />
             </div>
         </div>
     );
