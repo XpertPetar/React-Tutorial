4ba64e540c6f4608d089c008baf81224f9c88f61
Petar Josifoski <josifoski.petar@gmail.com>
2024-07-11 19:47:32 +0200

    add employee cards

--- src/App.js
+++ src/App.js
@@ -1,27 +1,85 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
     const showEmloyees = true;
-    const [role, setRole] = useState("Dev");
+    //const [role, setRole] = useState("Dev");
+    const [employees, setEmployees] = useState([
+        {
+            name: "Petar",
+            role: "Admin",
+            img: "https://images.pexels.com/photos/3778966/pexels-photo-3778966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            name: "Stefan",
+            role: "Dev",
+            img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            name: "Random",
+            role: "",
+            img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+        },
+        {
+            name: "Baci",
+            role: "Bot",
+            img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            name: "Random",
+            role: "",
+            img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+        },
+        {
+            name: "Zoki",
+            role: "Dev",
+            img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            name: "Bazo",
+            role: "Tester",
+            img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            name: "Random",
+            role: "",
+            img: "https://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+        }
+    ]);
     return (
         <div className="App">
             <header className="App-header">
                 {showEmloyees ? (
                     <>
-                        <label for="roleInput">Input Stefan's role: </label>
+                        {/* <label for="roleInput">Input Stefan's role: </label>
                         <input
+                            class="border-2 rounded"
                             id="roleInput"
                             onChange={(e) => {
                                 setRole(e.target.value);
                             }}
-                        ></input>
+                        ></input> */}
 
-                        <div class="bg-blue-300">
-                            <Employee name="Petar" role="Admin" />
-                            <Employee name="Stefan" role={role} />
-                            <Employee name="Random" />
+                        <div className="p-5 flex flex-wrap gap-2 justify-center">
+                            {employees.map((employee) => {
+                                return (
+                                    <Employee
+                                        key={uuidv4()}
+                                        name={employee.name}
+                                        role={employee.role}
+                                        img={employee.img}
+                                        alt="Employee Image"
+                                    ></Employee>
+                                );
+                            })}
+                        </div>
+
+                        <div className="flex justify-center mt-5">
+                            <button className="bg-transparent w-60 h-16 p-1 border rounded text-lg font-bold text-blue-500">
+                                + New Employee
+                            </button>
                         </div>
                     </>
                 ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,13 +1,20 @@
 function Employee(props) {
     return (
-        <>
-            <h2>Employee {props.name}</h2>
-            {props.role ? (
-                <span class="role_assigned">Role: {props.role}</span>
-            ) : (
-                <span class="role_na">Role: N/A</span>
-            )}
-        </>
+        <div className="h-64 w-3/4 m-3 justify-center py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img
+                className="block mx-auto h-24 object-cover h-[100px] w-[100px] rounded-full sm:mx-0 sm:shrink-0"
+                src={props.img}
+            />
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5 text-center">
+                    <p className="text-lg text-black font-semibold">{props.name}</p>
+                    <p className="text-slate-500 font-medium">{props.role ? props.role : "N/A"}</p>
+                </div>
+                <button className="px-4 py-1 w-full text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                    Update
+                </button>
+            </div>
+        </div>
     );
 }
 
